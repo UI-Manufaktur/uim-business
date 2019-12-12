aeccda45ffc79115d5a96006d30724d840210a8a
uim <ui-manufaktur@gmail.com>
2019-12-12 22:14:15 +0100

    First classes

---
+++ source/object_.d
@@ -0,0 +1,11 @@
+module uim.business.object_;
+
+class DBUSObject {
+  /*
+  id	Unique identifier of the invoice.	
+  createdOn	Date and time when the record was created.	
+  createdBy	Unique identifier of the user who created the record.	
+  modifiedOn	Date and time when the record was modified.	
+  modifiedBy	Unique identifier of the user who modified the record.
+  */
+}
